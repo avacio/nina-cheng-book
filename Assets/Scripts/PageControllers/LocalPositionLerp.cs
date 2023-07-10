1609c8664e8734865ec5a679fed0f6e546ab3d3e
Alexis Gervacio <alexis.gervacio@unity3d.com>
2023-07-10 01:15:23 -0700

    Page 17-18 simple blinking

--- Assets/Scripts/PageControllers/LocalPositionLerp.cs
+++ Assets/Scripts/PageControllers/LocalPositionLerp.cs
@@ -32,41 +32,4 @@ public class LocalPositionLerp : MonoBehaviour
                 : pointA;
         }
     }
-    //
-    // void Update()
-    // {
-    //     // Move our position a step closer to the target.
-    //     var step =  speed * Time.deltaTime; // calculate distance to move
-    //     transform.position = Vector3.MoveTowards(transform.position, currentDst, step);
-    //
-    //     // Check if the position of the cube and sphere are approximately equal.
-    //     if (Vector3.Distance(transform.position, currentDst) < 0.1f)
-    //     {
-    //         // Swap the position of the cylinder.
-    //         currentDst = currentDst == pointA
-    //             ? pointB
-    //             : pointA;
-    //     }
-    // }
-
-    // [SerializeField] private float timeDurationOneWay = 3f;
-    // [SerializeField] private Vector3 dstPosition;
-    // [SerializeField] private Vector3 srcPosition;
-    //
-    // void Start()
-    // {
-    //     StartCoroutine(LerpPosition(dstPosition, timeDurationOneWay));
-    // }
-    // IEnumerator LerpPosition(Vector3 targetPosition, float duration)
-    // {
-    //     float time = 0;
-    //     Vector3 startPosition = transform.position;
-    //     while (time < duration)
-    //     {
-    //         transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
-    //         time += Time.deltaTime;
-    //         yield return null;
-    //     }
-    //     transform.position = targetPosition;
-    // }
 }
---
+++ Assets/Scripts/PageControllers/SpriteReplace.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteReplace : MonoBehaviour
+{
+    [SerializeField] private Image image = null;
+    [SerializeField] private Sprite[] sprites = null;
+    [SerializeField] private float timePerSprite = 2f;
+
+    private int currentSprite = 0;
+
+    private void Reset()
+    {
+        if (image == null)
+        {
+            image = GetComponent<Image>();
+        }
+    }
+
+    private void Start()
+    {
+        if (image)
+        {
+            StartCoroutine(SpriteReplaceCoroutine());
+        }
+    }
+
+    IEnumerator SpriteReplaceCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timePerSprite);
+            int nextIndex = (currentSprite + 1) % sprites.Length;
+            image.sprite = sprites[nextIndex];
+            currentSprite = nextIndex;
+        }
+    }
+}
